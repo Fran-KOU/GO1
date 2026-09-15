3c668fa5d3b76247fa753376d952247269928f33
Fran-KOU <fracunapa@uide.edu.ec>
2026-09-15 17:18:25 +0000

    mostrarEncabeza

--- monitor_latencias.go
+++ monitor_latencias.go
@@ -2,5 +2,20 @@ package main
 
 import (
 	"fmt"
-	"errors"
+	
 )
+
+func main() {
+	mostrarEncabezado()
+	mostrarLatencia("web-01", 120.0)
+}
+
+func mostrarEncabezado() {
+	fmt.Println("-- Reportes de Latencias --")
+} // Salida Paso 1: -- Reportes de Latencias --
+
+func mostrarLatencia(servidor string, ms float64) {
+	fmt.Printf("Servidor: %s - Latencia %.2f ms/n", servidor, ms)
+	fmt.Println()
+}
+
